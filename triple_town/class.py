6a0886435fe4b332ffdcbb59c7efa4b267b86309
alexis bernard <bernalex49@gmail.com>
2024-04-18 14:50:08 +0200

    divers class

--- triple_town/class.py
+++ triple_town/class.py
@@ -2,120 +2,148 @@ import pygame
 import random
 pygame.display.set_caption("Triple Town")
 
+
+
+
+#==================================================================================================================
+#===============================================     GRILLE     ===================================================
+#==================================================================================================================
+
 class Grille:
-
     def __init__(self, screen):
-
         self.screen = screen
         self.cases = []
-        taille_case = 150
-        for y in range(5): # nombre de lignes
-            for x in range(5):  # nombre de colonnes
-
+        taille_case = 50
+        for y in range(15): # nombre de lignes
+            for x in range(15):  # nombre de colonnes
                 case_x = x * taille_case 
                 case_y = y * taille_case
-
-                # Coordonnées de chaques coins de la case
                 case = ((case_x, case_y), (case_x + taille_case, case_y), (case_x + taille_case, case_y + taille_case), (case_x, case_y + taille_case))
-
-                # On ajoute chaque cases dans une liste
                 self.cases.append(case)
 
     def afficher(self):
-
-        # Pour toutes les cases de la liste, on les dessine
-        for i in range (len(self.cases)) :
+        for i in range(len(self.cases)):
             pygame.draw.polygon(self.screen, (0, 0, 0), self.cases[i], 2)
-          # pygame.draw.polygon(taille écran, couleur, cotés du polygone, épaisseur traits dessin)
 
-    
     def case(self):
-
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-                 
                 position_souris = pygame.mouse.get_pos()
-                case_x = position_souris[0] // 150
-                case_y = position_souris[1] // 150
+                case_x = position_souris[0] // 50
+                case_y = position_souris[1] // 50
                 if case_x < 5.0:
                     print(f"Vous avez choisi la case ({case_x}, {case_y})")
 
 
+#==================================================================================================================
+#===============================================     ITEMS     ====================================================
+#==================================================================================================================
+
+
 class Items:
 
+
     def __init__(self):
-        
-        self.pierre=pygame.image.load("triple_town/img/pierre.png").convert_alpha()
-        self.rocher=pygame.image.load("triple_town/img/rocher.png").convert_alpha()
-        self.eglise=pygame.image.load("triple_town/img/eglise.png").convert_alpha()
-        self.cathedrale=pygame.image.load("triple_town/img/cathedrale.png").convert_alpha()
-        self.herbe=pygame.image.load("triple_town/img/herbe.png").convert_alpha()
-        self.buisson=pygame.image.load("triple_town/img/buisson.png").convert_alpha()
-        self.arbre=pygame.image.load("triple_town/img/arbre.png").convert_alpha()
-        self.cabane=pygame.image.load("triple_town/img/cabane.png").convert_alpha()
-        self.maison=pygame.image.load("triple_town/img/maison.png").convert_alpha()
-        self.villa=pygame.image.load("triple_town/img/villa.png").convert_alpha()
-        self.chateau=pygame.image.load("triple_town/img/chateau.png").convert_alpha()
-        self.chateaumagique=pygame.image.load("triple_town/img/chateaumagique.png").convert_alpha()
+        self.pierre = pygame.image.load("triple_town/img/pierre.png").convert_alpha()
+        self.rocher = pygame.image.load("triple_town/img/rocher.png").convert_alpha()
+        self.eglise = pygame.image.load("triple_town/img/eglise.png").convert_alpha()
+        self.cathedrale = pygame.image.load("triple_town/img/cathedrale.png").convert_alpha()
+        self.herbe = pygame.image.load("triple_town/img/herbe.png").convert_alpha()
+        self.buisson = pygame.image.load("triple_town/img/buisson.png").convert_alpha()
+        self.arbre = pygame.image.load("triple_town/img/arbre.png").convert_alpha()
+        self.cabane = pygame.image.load("triple_town/img/cabane.png").convert_alpha()
+        self.maison = pygame.image.load("triple_town/img/maison.png").convert_alpha()
+        self.villa = pygame.image.load("triple_town/img/villa.png").convert_alpha()
+        self.chateau = pygame.image.load("triple_town/img/chateau.png").convert_alpha()
+        self.chateaumagique = pygame.image.load("triple_town/img/chateaumagique.png").convert_alpha()
 
         self.liste_items = []
+        self.positions_curseur = []  # Liste pour stocker les positions du curseur
 
     def liste(self):
 
-        for i in range (20):
+        self.liste_items = []
+
+        for i in range(20):
             self.liste_items.append(self.herbe)
-
-        for i in range (15):
+        for i in range(15):
             self.liste_items.append(self.pierre)
-
-        for i in range (10):
+        for i in range(10):
             self.liste_items.append(self.buisson)
 
-        return random.shuffle(self.liste_items)
+        random.shuffle(self.liste_items)
+        return self.liste_items
 
-    
     def suivant(self):
-
+        
         return self.liste_items[0]
 
 
+
+#==================================================================================================================
+#===============================================     GAME     =====================================================
+#==================================================================================================================
+
+
 class Game:
-
     def __init__(self):
-
         self.screen = pygame.display.set_mode((1000,750))
         self.running = True
         self.grille = Grille(self.screen)
         self.items = Items()
+        self.liste_items = self.items.liste()  # On initialise la liste
+        self.piece_suivante = self.liste_items.pop(0) # Premiere pièce que l'on prend et supprime
 
     def jeu(self):
+        positions_curseur = []  # Liste pour stocker les positions du curseur  
 
-        # Chargement et affichage des images
-        fond_jeu = pygame.image.load("triple_town/img/fond.jpg")
-        fond_jeu_r = pygame.transform.scale(fond_jeu,(750,750))
-        self.screen.blit(fond_jeu_r,(0,0))
+        while self.running:
+            curseur = self.piece_suivante
 
-        fond_score = pygame.image.load("triple_town/img/fond_score.png")
-        fond_score_r = pygame.transform.scale(fond_score,(736,750))
-        self.screen.blit(fond_score_r,(750,0))       
+            for pos in positions_curseur:
+                self.screen.blit(curseur, pos)
 
-        while self.running == True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-                    
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.grille.case()
+                    positions_curseur.append(event.pos)
+                    if self.liste_items:  # Vérifie si la liste mélangée n'est pas vide
+                        self.piece_suivante = self.liste_items.pop(0)  # On récupère la pièce suivante (premiere de la liste que l'on supprime)
+                    else:
+                        self.piece_suivante = None 
+
+            fond_jeu = pygame.image.load("triple_town/img/fond.jpg")
+            fond_jeu_r = pygame.transform.scale(fond_jeu, (750, 750))
+            self.screen.blit(fond_jeu_r, (0, 0))
+
+            fond_score = pygame.image.load("triple_town/img/fond_score.png")
+            fond_score_r = pygame.transform.scale(fond_score, (736, 750))
+            self.screen.blit(fond_score_r, (750, 0)) 
 
             self.grille.afficher()
-            self.grille.case()
 
-            self.items.liste()
-            self.screen.blit(self.items.suivant(),(850,110))
-                
-            pygame.display.flip() # mise à jour de la page 
-        
+            pygame.mouse.set_visible(False)
+            if self.piece_suivante:
+                self.screen.blit(self.piece_suivante, pygame.mouse.get_pos())
+
+            for pos in positions_curseur:
+                self.screen.blit(curseur, pos)
+
+            if self.piece_suivante:
+                self.screen.blit(self.piece_suivante, (850, 110))
+
+            pygame.display.flip()
 
         pygame.quit()
 
+
+
+
+#==================================================================================================================
+#===============================================     MAIN     =====================================================
+#==================================================================================================================
 
 
 

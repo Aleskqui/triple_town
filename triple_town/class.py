8b872ae5824ec54105f11db8a3399ee1195bb5f3
guillaume4922 <167083860+guillaume4922@users.noreply.github.com>
2024-04-26 16:38:20 +0200

    Update class.py
    ajout grille/console

--- triple_town/class.py
+++ triple_town/class.py
@@ -1,7 +1,9 @@
 import pygame
 import random
-import numpy as np 
+import numpy as np
 import pygame.mixer
+
+
 pygame.display.set_caption("Triple Town")
 
 
@@ -10,76 +12,65 @@ pygame.display.set_caption("Triple Town")
 #==================================================================================================================
 
 
-
 class Accueil:
 
-    
     def __init__(self):
-        self.screen = pygame.display.set_mode((1000,750))
+        self.screen = pygame.display.set_mode((1000, 750))
         # Ecran d'accueil
         self.lancement = pygame.image.load("triple_town/img/home.png")
 
         # Bouton Play
         self.btn_play = pygame.image.load("triple_town/img/play.png")
-        self.pos_play = self.btn_play.get_rect(topleft=(400, 300)) # On recupère l'emplacement (le rectangle rect) du btn play
+        self.pos_play = self.btn_play.get_rect(topleft=(400, 300))  # On récupère l'emplacement (le rectangle rect) du btn play
 
         # Bouton Règles
         self.btn_regles = pygame.image.load("triple_town/img/btn_regles.png")
-        self.pos_regles = self.btn_regles.get_rect(topleft=(320, 425)) # On recupère l'emplacement (le rectangle rect) du btn regles
+        self.pos_regles = self.btn_regles.get_rect(topleft=(320, 425))  # On récupère l'emplacement (le rectangle rect) du btn règles
 
         # Bouton Retour
         self.btn_retour = pygame.image.load("triple_town/img/retour.png")
-        self.pos_retour = self.btn_retour.get_rect(topleft=(890, 20)) # On recupère l'emplacement (le rectangle rect) du btn regles
+        self.pos_retour = self.btn_retour.get_rect(topleft=(890, 20))  # On récupère l'emplacement (le rectangle rect) du btn regles
 
         # Règles
         self.regles = pygame.image.load("triple_town/img/regles.png")
 
-        #Son
-        self.son= Son()  # Ajout du lecteur audio
-
+        # Son
+        self.son = Son()  # Ajout du lecteur audio
 
     def afficher(self):
-        self.screen.blit(self.lancement,(0,0))
-        self.screen.blit(self.btn_play,(400,300))
-        self.screen.blit(self.btn_regles,(320,425))
+        self.screen.blit(self.lancement, (0, 0))
+        self.screen.blit(self.btn_play, (400, 300))
+        self.screen.blit(self.btn_regles, (320, 425))
         self.son.lire_audio("triple_town/sounds/accueil.mp3")
         pygame.display.flip()
 
-
-
     def en_cours(self):
-
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
-                    pygame.quit()
-
+                pygame.quit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
 
                 # On vérifie si le btn play est cliqué
                 if self.pos_play.collidepoint(mouse_pos):
                     return False  # Si c'est le cas on quitte l'accueil
-                
+
                 # On vérifie si le btn règles est cliqué
                 elif self.pos_regles.collidepoint(mouse_pos):
                     self.son.fermer_audio("triple_town/sounds/accueil.mp3")
-                    
                     self.screen.blit(self.regles, (0, 0))  # Si c'est le cas on affiche les règles
                     self.screen.blit(self.btn_retour, (890, 20))
                     pygame.display.flip()
 
                 elif self.pos_retour.collidepoint(mouse_pos):
-                    self.pieces_placees = []
                     self.afficher()
 
-        return True  # L'écran d'accueil reste affiché 
+        return True  # L'écran d'accueil reste affiché
 
 
 #==================================================================================================================
 #===============================================     SONS    ===================================================
 #==================================================================================================================
-
 
 
 class Son:
@@ -89,8 +80,8 @@ class Son:
     def lire_audio(self, nom_fichier):
         pygame.mixer.music.load(nom_fichier)
         pygame.mixer.music.play()
-        
-    def fermer_audio(self,nom_fichier):
+
+    def fermer_audio(self, nom_fichier):
         pygame.mixer.music.load(nom_fichier)
         pygame.mixer.music.stop()
 
@@ -99,28 +90,27 @@ class Son:
 #===============================================     GRILLE     ===================================================
 #==================================================================================================================
 
+
 class Grille:
     def __init__(self, taille_x, taille_y):
         self.taille_x = taille_x
         self.taille_y = taille_y
-        self.screen = pygame.display.set_mode((1000,750))
+        self.screen = pygame.display.set_mode((1000, 750))
         self.cases = []
-        self.grille = np.zeros((taille_x,taille_y), dtype=object)
+        self.grille = np.zeros((taille_x, taille_y), dtype=object)
         self.panier = None
-        taille_case = 750/taille_x 
-        for y in range(taille_y): # nombre de lignes
+        taille_case = 750 / taille_x
+        for y in range(taille_y):  # nombre de lignes
             for x in range(taille_x):  # nombre de colonnes
-                case_x = x * taille_case 
+                case_x = x * taille_case
                 case_y = y * taille_case
-                case = ((case_x, case_y), (case_x + taille_case, case_y), (case_x + taille_case, case_y + taille_case), (case_x, case_y + taille_case))
+                case = ((case_x, case_y), (case_x + taille_case, case_y), (case_x + taille_case, case_y + taille_case),
+                        (case_x, case_y + taille_case))
                 self.cases.append(case)
 
     def afficher(self):
         for i in range(len(self.cases)):
             pygame.draw.polygon(self.screen, (0, 0, 0), self.cases[i], 2)
-
-
-            
 
     def case(self):
         for event in pygame.event.get():
@@ -131,15 +121,10 @@ class Grille:
                 if case_x < 5.0:
                     print(f"Vous avez choisi la case ({case_x}, {case_y})")
 
-
-
-    
     # Place un élément à une position donnée dans la grille
     def placer_element(self, element, x, y):
-        if x <= self.taille_x and y <= self.taille_y :
+        if x < self.taille_x and y < self.taille_y:
             self.grille[x, y] = element
-        else:  
-            pass
 
     # Supprime un élément à une position donnée dans la grille
     def supprimer_elements(self, x, y, taille):
@@ -147,8 +132,6 @@ class Grille:
             for j in range(y - taille + 1, y + 1):
                 if 0 <= i < self.taille_x and 0 <= j < self.taille_y:
                     self.grille[i, j] = None
-
-
 
     # Vérifie s'il y a un alignement de 3 éléments identiques (horizontalement ou verticalement) à partir de la position (x, y).
     # Elle renvoie l'élément aligné et la taille de l'alignement (3 dans ce cas).
@@ -165,9 +148,6 @@ class Grille:
         if x + 2 < self.taille_x and y - 2 >= 0 and self.grille[x, y] == self.grille[x + 1, y - 1] == self.grille[x + 2, y - 2]:
             return self.grille[x, y], 3
         return None, 0
-    
-
-
 
     def afficher_grille_console(self):
         # Affiche la grille dans la console
@@ -184,19 +164,6 @@ class Grille:
         if self.panier is not None:
             print(f"Panier: {self.panier}")
 
-# Création de la grille de jeu
-grille = Grille(5,5)
-
-# Placement d'éléments dans la grille
-grille.placer_element("P", 0, 0)
-grille.placer_element("R", 1, 1)
-grille.placer_element("E", 2, 2)
-grille.placer_element("H", 3, 3)
-grille.placer_element("H", 4, 3)
-grille.placer_element("H", 2, 3)
-
-grille.afficher_grille_console()
-
 
 #==================================================================================================================
 #===============================================     ITEMS     ====================================================
@@ -204,7 +171,6 @@ grille.afficher_grille_console()
 
 
 class Items:
-
 
     def __init__(self):
         self.pierre = pygame.image.load("triple_town/img/pierre.png").convert_alpha()
@@ -223,25 +189,29 @@ class Items:
         self.liste_items = []
         self.positions_curseur = []  # Liste pour stocker les positions du curseur
 
+        self.piece_initiales = {
+            "P": pygame.image.load("triple_town/img/pierre.png").convert_alpha(),
+            "R": pygame.image.load("triple_town/img/rocher.png").convert_alpha(),
+            "E": pygame.image.load("triple_town/img/eglise.png").convert_alpha(),
+            "C": pygame.image.load("triple_town/img/cathedrale.png").convert_alpha(),
+            "H": pygame.image.load("triple_town/img/herbe.png").convert_alpha(),
+            "B": pygame.image.load("triple_town/img/buisson.png").convert_alpha(),
+            "A": pygame.image.load("triple_town/img/arbre.png").convert_alpha(),
+            "CA": pygame.image.load("triple_town/img/cabane.png").convert_alpha(),
+            "M": pygame.image.load("triple_town/img/maison.png").convert_alpha(),
+            "V": pygame.image.load("triple_town/img/villa.png").convert_alpha(),
+            "CH": pygame.image.load("triple_town/img/chateau.png").convert_alpha(),
+            "CM": pygame.image.load("triple_town/img/chateaumagique.png").convert_alpha()
+        }
+
     def liste(self):
-
-        self.liste_items = []
-
-        for i in range(20):
-            self.liste_items.append(self.herbe)
-        for i in range(15):
-            self.liste_items.append(self.pierre)
-        for i in range(10):
-            self.liste_items.append(self.buisson)
-
+        self.liste_items = ["P", "R", "E", "C", "H", "A", "CA", "M", "V", "CH", "CM"]  # Exemple avec seulement deux pièces
         random.shuffle(self.liste_items)
         return self.liste_items
 
     def suivant(self):
-        
         return self.liste_items[0]
-    
-    
+
 
 #==================================================================================================================
 #===============================================     GAME     =====================================================
@@ -250,36 +220,27 @@ class Items:
 
 class Game:
 
-
     def __init__(self):
-        self.screen = pygame.display.set_mode((1000,750))
+        self.screen = pygame.display.set_mode((1000, 750))
         self.running = True
-        self.grille = Grille(5,5)
+        self.grille = Grille(5, 5)
         self.accueil = Accueil()
         self.items = Items()
-        
 
         self.liste_items = self.items.liste()  # On initialise la liste
-        self.piece_suivante = self.liste_items.pop(0) # Premiere pièce que l'on prend et supprime
-        self.pieces_placees = [] # Liste pour stocker les positions des pieces dejà placées  
+        self.piece_suivante = self.liste_items.pop(0)  # Première pièce que l'on prend et supprime
+        self.pieces_placees = []  # Liste pour stocker les positions des pièces déjà placées
 
         self.btn_retour = pygame.image.load("triple_town/img/retour.png")
-        self.pos_retour = self.btn_retour.get_rect(topleft=(890, 20)) # On recupère l'emplacement (le rectangle rect) du btn retour
-        self.son= Son()  # Ajout du lecteur audio
+        self.pos_retour = self.btn_retour.get_rect(topleft=(890, 20))  # On récupère l'emplacement (le rectangle rect) du btn retour
+        self.son = Son()  # Ajout du lecteur audio
 
-
-
-
-    def score(self,compt):
-
+    def score(self, compt):
 
         # Affichage du score courant
         font = pygame.font.Font(None, 36)
-        score_text = font.render(f"Score: {compt}", True, (255,255,0))
+        score_text = font.render(f"Score: {compt}", True, (255, 255, 0))
         self.screen.blit(score_text, (800, 120))
-        
-
-
 
     def jeu(self):
         self.son.lire_audio("triple_town/sounds/aventure.mp3")
@@ -287,8 +248,8 @@ class Game:
         self.accueil = Accueil()
 
         while self.running:
-            
-            curseur = self.piece_suivante
+
+            curseur = self.items.piece_initiales[self.piece_suivante]
             compt = 0
 
             for pos in positions_curseur:
@@ -300,82 +261,69 @@ class Game:
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_pos = pygame.mouse.get_pos()
 
-
                     # Gestion des clics dans la zone grise
-                    
-                    if mouse_pos[0] > 750 : # zone score etc... 
-                        
-                        if accueil.pos_retour.collidepoint(mouse_pos):
+                    if mouse_pos[0] > 750:  # zone score etc...
+                        if self.accueil.pos_retour.collidepoint(mouse_pos):
                             self.accueil.afficher()
 
-
-                    # Gestion des clics dans la zone de jeu 
-
+                    # Gestion des clics dans la zone de jeu
                     else:
-
-                        # On calcul les coordonnés de la case avc la position de la souris
-                        case_x = mouse_pos[0] // (750 / self.grille.taille_x) 
-                        case_y = mouse_pos[1] // (750 / self.grille.taille_y)  
-                        self.grille.placer_element(self.piece_suivante, int(case_x), int(case_y))  # Placer l'élément sur la grille
+                        # On calcule les coordonnées de la case avec la position de la souris
+                        case_x = mouse_pos[0] // (750 / self.grille.taille_x)
+                        case_y = mouse_pos[1] // (750 / self.grille.taille_y)
+                        self.grille.placer_element(self.piece_suivante[0], int(case_x), int(case_y))  # Placer l'élément sur la grille
                         self.grille.afficher_grille_console()  # Ajouter la pièce dans la grille de la console
                         positions_curseur.append(event.pos)
                         compt += 1
-
                         if self.liste_items:
                             self.pieces_placees.append(self.piece_suivante)
                             self.piece_suivante = self.liste_items.pop(0)
                         else:
                             self.piece_suivante = None
 
-            #----------------------------------------------------------------------------
+            # ------------------------------------------------------------------------------
             fond_jeu = pygame.image.load("triple_town/img/fond.jpg")
             fond_jeu_r = pygame.transform.scale(fond_jeu, (750, 750))
             self.screen.blit(fond_jeu_r, (0, 0))
 
             fond_score = pygame.image.load("triple_town/img/fond_score.png")
             fond_score_r = pygame.transform.scale(fond_score, (736, 750))
-            self.screen.blit(fond_score_r, (750, 0)) 
+            self.screen.blit(fond_score_r, (750, 0))
 
-            self.screen.blit(self.btn_retour,(890,20))
-            #----------------------------------------------------------------------------
+            self.screen.blit(self.btn_retour, (890, 20))
+            # ------------------------------------------------------------------------------
 
+            self.grille.afficher()  # On affiche la grille
 
-            self.grille.afficher() # On affiche la grille
+            # ------------------------------------------------------------------------------
 
-
-            #----------------------------------------------------------------------------
-
-            partie_grise = (750,0)
+            partie_grise = (750, 0)
             if pygame.mouse.get_pos() < partie_grise:
-                pygame.mouse.set_visible(False) # On rend invisible le curseur par défaut
+                pygame.mouse.set_visible(False)  # On rend invisible le curseur par défaut
 
-                # Si il y a une piece à placer
+                # Si il y a une pièce à placer
                 if self.piece_suivante:
                     # Le curseur prend la forme de cette pièce
-                    self.screen.blit(self.piece_suivante, pygame.mouse.get_pos())
+                    self.screen.blit(curseur, pygame.mouse.get_pos())
 
             else:
                 pygame.mouse.set_visible(True)
 
-
-            
-
-            # Pour chaque endroit ou l'on à cliqué 
-            for i in range(len(positions_curseur)):
-                pos = positions_curseur[i] # On prend la position
-                piece = self.pieces_placees[i] # On recupère la piece que l'on a souhaité déposé
-                self.screen.blit(piece, pos) # On l'a dessine
+            # Pour chaque endroit où l'on a cliqué
+            for i in range(min(len(positions_curseur), len(self.pieces_placees))):
+                pos = positions_curseur[i]  # On prend la position
+                piece = self.pieces_placees[i]  # On récupère la pièce que l'on a souhaité déposer
+                piece_surface = self.items.piece_initiales[piece]  # Récupérer la surface correspondante à partir du dictionnaire
+                self.screen.blit(piece_surface, pos)  # Dessiner la surface de la pièce à la position donnée
 
                 compt += 1
 
             if self.piece_suivante:
                 font = pygame.font.Font(None, 36)
-                texte_suivant = font.render("Item à placer :", True, (255,255,0))
+                texte_suivant = font.render("Item à placer :", True, (255, 255, 0))
                 self.screen.blit(texte_suivant, (800, 180))
-                self.screen.blit(self.piece_suivante, (850, 210))
-            #----------------------------------------------------------------------------
-
-
+                self.screen.blit(curseur, (850, 210))
+            # ------------------------------------------------------------------------------
 
             self.score(compt)
 
@@ -384,17 +332,12 @@ class Game:
         pygame.quit()
 
 
-
-
-
-
 #==================================================================================================================
 #===============================================     MAIN     =====================================================
 #==================================================================================================================
 
 
-
-if __name__ == '__main__': 
+if __name__ == '__main__':
     pygame.init()
     accueil = Accueil()
     running = False
@@ -408,12 +351,3 @@ if __name__ == '__main__':
     game.jeu()
 
     pygame.quit()
-
-
-
-
-
-
-
-
-
